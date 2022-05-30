f52e77ee572d0255f7718789c9c57fce2afd2bb4
Malcolm Keyes <3.malcolmk@gmail.com>
2022-05-30 17:10:51 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    # read through https://www.geeksforgeeks.org/ruby-regular-expressions/
+    # for help with regex!
+    # looks for a block of 0+ consecutive !?. (in any order) followed by 1+ space(s)
+    self.split(/[!*?*.*] +/).size
   end
 end
